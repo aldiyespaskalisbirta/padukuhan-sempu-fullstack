b6932f8eb38907f28196e98f2ea50dab09a7f04e
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2023-07-16 14:45:21 +0700

    fix/models

--- padukuhan-sempu-backend/models/Image.js
+++ padukuhan-sempu-backend/models/Image.js
@@ -6,10 +6,18 @@ const { DataTypes } = Sequelize;
 const Images = db.define(
   "Images",
   {
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    image: DataTypes.STRING,
-    url: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { noEmpty: true, len: [3, 100] },
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { noEmpty: true, isEmail: true },
+    },
+    image: { type: DataTypes.STRING, allowNull: false },
+    url: { type: DataTypes.STRING },
   },
   {
     freezeTableName: true,
