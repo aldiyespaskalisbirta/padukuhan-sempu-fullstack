4e19a64edf168645c13330bd913d4a1c508d40ce
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2023-07-17 13:35:56 +0700

    with credential true

--- padukuhan-sempu-frontend/src/main.jsx
+++ padukuhan-sempu-frontend/src/main.jsx
@@ -4,6 +4,10 @@ import "./styles/index.css";
 import router from "./router";
 import { RouterProvider } from "react-router-dom";
 import { AuthContextProvider } from "./contex/authContextProvider";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
