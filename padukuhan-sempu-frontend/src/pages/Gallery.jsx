c3003950c6790fa561a8956a8ee6c4a8f17cf9da
Hallo Bap! <sandyyuan53@gmail.com>
2023-07-19 17:05:19 +0700

    cok

--- padukuhan-sempu-frontend/src/pages/Gallery.jsx
+++ padukuhan-sempu-frontend/src/pages/Gallery.jsx
@@ -1,6 +1,12 @@
+import React, { useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { AuthContext } from "../contex/authContextProvider";
 
 const Gallery = () => {
+
+  const { token } = useContext(AuthContext);
+  const isLoggedIn = !!token
+
   return (
     <div className="mt-16">
       <div className="flex justify-between text-center mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
@@ -8,12 +14,15 @@ const Gallery = () => {
           GALLERY FOTO
         </h1>
 
-        <Link
-          to="/gallery/add-image"
-          className="hover:bg-gray-50 text-gray-900 font-semibold shadow rounded px-4 py-2 my-auto text-center"
-        >
-          add image
-        </Link>
+        {isLoggedIn && (
+          <Link
+            to="/gallery/add-image"
+            className="hover:bg-gray-50 text-gray-900 font-semibold shadow rounded px-4 py-2 my-auto text-center"
+          >
+            add image
+          </Link>
+
+        )}
       </div>
       <main className="min-h-screen">
         <div className="flex justify-center items-center mx-auto max-w-7xl sm:px-6 lg:px-8">
