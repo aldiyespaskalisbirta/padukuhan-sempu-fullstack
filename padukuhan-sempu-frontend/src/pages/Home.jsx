0b9e98962601d3e1cd0ea95bff144677a88a516b
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2023-07-20 01:01:38 +0700

    line clamp

--- padukuhan-sempu-frontend/src/pages/Home.jsx
+++ padukuhan-sempu-frontend/src/pages/Home.jsx
@@ -77,6 +77,8 @@ const Home = () => {
         <div className="flex justify-center items-center">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
             <Fade direction="down">
+              {/* BUAT TITIK TIGA (...) PAKAI TAILWIND LINE-CLAMP */}
+              {/* https://tailwindcss.com/docs/line-clamp */}
               <Card
                 url={"https://flowbite.com/docs/images/carousel/carousel-1.svg"}
                 judul={"Rasulan"}
